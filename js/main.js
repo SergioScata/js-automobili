983285cff301a645a28bb4e092c3412f92aa7fd7
SergioScata <sergiopio395@gmail.com>
2023-11-22 14:31:05 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+let automobili = [
+    {marca = "Audi",
+     modello = "Q3",
+     alimentazione = "Diesel"
+},
+{marca = "Fiat",
+     modello = "500",
+     alimentazione = "Benzina"
+},
+{marca = "Mercedes",
+     modello = "Benz",
+     alimentazione = "Benzina"
+},
+{marca = "Audi",
+     modello = "Q3",
+     alimentazione = "Diesel"
+},
+{marca = "Audi",
+     modello = "Q3",
+     alimentazione = "Diesel"
+},
+{marca = "Audi",
+     modello = "Q3",
+     alimentazione = "Diesel"
+},
+{marca = "Audi",
+     modello = "Q3",
+     alimentazione = "Diesel"
+},
+{marca = "Audi",
+     modello = "Q3",
+     alimentazione = "Diesel"
+},
+{marca = "Audi",
+     modello = "Q3",
+     alimentazione = "Diesel"
+},
+{marca = "Audi",
+     modello = "Q3",
+     alimentazione = "Diesel"
+}
+]
