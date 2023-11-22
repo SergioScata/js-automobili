d4d0f3bcaa925991594dc8d25b68c448a69b32bd
SergioScata <sergiopio395@gmail.com>
2023-11-22 15:28:46 +0100

    Updated Project

--- js/main.js
+++ js/main.js
@@ -1,42 +1,98 @@
 let automobili = [
-    {marca = "Audi",
-     modello = "Q3",
-     alimentazione = "Diesel"
+    {marca : "Audi",
+     modello : "Q3",
+     alimentazione : "Diesel"
 },
-{marca = "Fiat",
-     modello = "500",
-     alimentazione = "Benzina"
+{marca : "Fiat",
+     modello : "500",
+     alimentazione : "Benzina"
 },
-{marca = "Mercedes",
-     modello = "Benz",
-     alimentazione = "Benzina"
+{marca : "Mercedes",
+     modello : "Benz",
+     alimentazione : "Benzina"
 },
-{marca = "Audi",
-     modello = "Q3",
-     alimentazione = "Diesel"
+{marca : "Ford",
+     modello : "Fiesta",
+     alimentazione : "Diesel"
 },
-{marca = "Audi",
-     modello = "Q3",
-     alimentazione = "Diesel"
+{marca : "Volkswagen",
+     modello : "Up",
+     alimentazione : "Elettrica"
 },
-{marca = "Audi",
-     modello = "Q3",
-     alimentazione = "Diesel"
+{marca : "Toyota",
+     modello : "Yaris",
+     alimentazione : "Gpl"
 },
-{marca = "Audi",
-     modello = "Q3",
-     alimentazione = "Diesel"
+{marca : "Tesla",
+     modello : "Model 3",
+     alimentazione : "Elettrica"
 },
-{marca = "Audi",
-     modello = "Q3",
-     alimentazione = "Diesel"
+{marca : "Honda",
+     modello : "Jetz",
+     alimentazione : "Diesel"
 },
-{marca = "Audi",
-     modello = "Q3",
-     alimentazione = "Diesel"
+{marca : "Hyundai",
+     modello : "Tucson",
+     alimentazione : "Benzina"
 },
-{marca = "Audi",
-     modello = "Q3",
-     alimentazione = "Diesel"
+{marca : "Mini",
+     modello : "Cooper",
+     alimentazione : "Diesel"
 }
-]
+];
+
+let AutoBenzina = [];
+let AutoDiesel = [];
+let AltreAuto = [];
+
+automobili.forEach (function (auto) {
+    if (auto.alimentazione == "Benzina"){
+        AutoBenzina.push(auto);
+    }
+    else if(auto.alimentazione == "Diesel"){
+        AutoDiesel.push(auto);
+    }
+    else{
+        AltreAuto.push(auto);
+    }
+});
+
+console.log(AutoBenzina);
+console.log(AutoDiesel);
+console.log(AltreAuto);
+
+document.getElementById("containerrow").innerHTML += "Automobili a benzina"
+for (let i= 0; i< AutoBenzina.length; i++){
+   document.getElementById("containerrow").innerHTML += `<div class="card">
+   <ul class="list-group list-group-flush">
+     <li class="list-group-item">${AutoBenzina[i].marca}</li>
+     <li class="list-group-item">${AutoBenzina[i].modello}</li>
+     <li class="list-group-item">${AutoBenzina[i].alimentazione}</li>
+   </ul>
+ </div>`
+}
+
+document.getElementById("containerrow").innerHTML += "Automobili a Diesel"
+for (let i= 0; i< AutoDiesel.length; i++){
+   document.getElementById("containerrow").innerHTML += `<div class="card">
+   <ul class="list-group list-group-flush">
+     <li class="list-group-item">${AutoDiesel[i].marca}</li>
+     <li class="list-group-item">${AutoDiesel[i].modello}</li>
+     <li class="list-group-item">${AutoDiesel[i].alimentazione}</li>
+   </ul>
+ </div>`
+}
+
+document.getElementById("containerrow").innerHTML += "Automobili con diversi tipi di alimentazione"
+for (let i= 0; i< AltreAuto.length; i++){
+   document.getElementById("containerrow").innerHTML += `<div class="card">
+   <ul class="list-group list-group-flush">
+     <li class="list-group-item">${AltreAuto[i].marca}</li>
+     <li class="list-group-item">${AltreAuto[i].modello}</li>
+     <li class="list-group-item">${AltreAuto[i].alimentazione}</li>
+   </ul>
+ </div>`
+}
+
+
+document.getElementById
